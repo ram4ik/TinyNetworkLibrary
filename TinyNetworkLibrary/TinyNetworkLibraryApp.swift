b8023ff6fad3d5fd83d5f893682cc06c1fe8d2bd
Ramill <ram4ik@hotmail.com>
2024-10-27 13:13:48 +0200

    Initial Commit

---
+++ TinyNetworkLibrary/TinyNetworkLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyNetworkLibraryApp.swift
+//  TinyNetworkLibrary
+//
+//  Created by admin on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TinyNetworkLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
